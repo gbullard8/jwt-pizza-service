09b41e20a2ec8e74ca8bd0988e449b2310af7b0e
gbullard1 <gbullard8@gmail.com>
2024-11-27 04:29:24 -0800

    fix(index.js) lint issue

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-const app = require('./service.js');
-
 const port = process.argv[2] || 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
 
 const express = require('express');
-app = express();
+const app = express();
 
 const metrics = require('./metrics');
 let greeting = 'hello';
