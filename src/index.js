e7c2c71d82feec22a81871a686703e6119c5df8b
gbullard1 <gbullard8@gmail.com>
2024-11-27 06:08:03 -0800

    update(metrics)

--- src/index.js
+++ src/index.js
@@ -1,30 +1,21 @@
-const express = require('express');
-const fetch = require('node-fetch');
+const app = require('./service.js'); 
+const metrics = require('./metrics'); 
 
-const router = express.Router();
+const port = process.argv[2] || 3000;
 
 
-router.get('/menu', async (req, res) => {
-  try {
-    const backendUrl = 'https://pizza-service.cs329jwtpizza.com/api/order/menu'; 
-
-    const response = await fetch(backendUrl);
-
-    if (!response.ok) {
-      throw new Error(`Failed to fetch menu: ${response.statusText}`);
-    }
-
-    const menuData = await response.json();
-
-    res.json(menuData);
-  } catch (error) {
-    console.error('Error fetching menu data:', error);
-    res.status(500).json({ error: 'Failed to fetch menu data' });
-  }
+app.use((req, res, next) => {
+  metrics.incrementRequests(req.method); 
+  next();
 });
 
-module.exports = router;
 
+
+
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
 
 
 
