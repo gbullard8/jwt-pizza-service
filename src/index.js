efa843d1f180f243ac75d15a2b3fb4b32ce98c78
gbullard1 <gbullard8@gmail.com>
2024-11-27 04:37:31 -0800

    fix(index.js) lint issue

--- src/index.js
+++ src/index.js
@@ -5,16 +5,17 @@ app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
 
+
 const metrics = require('./metrics');
 let greeting = 'hello';
 
-app.use(express.json());
+
 
 app.get('/hello/:name', (req, res) => {
   metrics.incrementRequests();
   res.send({ [greeting]: req.params.name });
 });
 
-app.listen(3000, function () {
-  console.log(`Listening on port 3000`);
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
